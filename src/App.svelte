4219115b2e12bc510ec3319e0ea037150431f5fa
Presum0 <preethamnelaturi@gmail.com>
2025-09-03 16:44:38 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import But from "./butt.svelte";
+  let a = $state(0);
+  let op = $state([1,2,3,4,5,6,7,8,9,0,'+','-','*','/','=']);
+  function handleClick(){
+
+  }
+</script>
+
+<main class="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-900 text-white">
+  <div class="block bg-black w-56 p-4 text-white rounded-xl box-border">
+    <input 
+      type="text" 
+      bind:value={a} 
+      class="w-full p-2 rounded bg-gray-800 text-white border border-gray-600 focus:border-blue-500 mb-1 focus:outline-none"
+    >
+    <div class="grid grid-cols-4 gap-1 mt-2">
+      <But items={op} />
+    </div> 
+    {#each op as i}
+      {#if i === '1'}
+        {a} = i;
+      {/if}
+    {/each}
+  </div>
+</main>
---
+++ src/butt.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let items = [];
+</script>
+
+{#each items as item}
+    <button 
+        on:click={()=> handleClick(item)}
+        class="m-1 p-2 w-10 h-10 flex items-center justify-center bg-gray-700 hover:bg-gray-600 rounded border-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+        {item}
+    </button>
+{/each}
