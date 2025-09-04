012f3261a0236824620be034c889c72e426708fb
Presum0 <preethamnelaturi@gmail.com>
2025-09-04 14:17:27 +0530

    mapped the buttons to the input

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
   import But from "./butt.svelte";
-  let a = $state(0);
-  let op = $state([1,2,3,4,5,6,7,8,9,0,'+','-','*','/','=']);
-  function handleClick(){
-
+  let inputValue = $state('0');
+  let op = $state([1,2,3,4,5,6,7,8,9,0,'+','-','*','/','=','c']);
+  function handleButtonClick(value) {
+    if (inputValue === '0' && value !== '.') {
+      inputValue = value.toString();
+    } else {
+      inputValue += value.toString();
+    }
   }
 </script>
 
@@ -11,16 +15,11 @@
   <div class="block bg-black w-56 p-4 text-white rounded-xl box-border">
     <input 
       type="text" 
-      bind:value={a} 
+      bind:value={inputValue} 
       class="w-full p-2 rounded bg-gray-800 text-white border border-gray-600 focus:border-blue-500 mb-1 focus:outline-none"
     >
     <div class="grid grid-cols-4 gap-1 mt-2">
-      <But items={op} />
+      <But items={op} onButtonClick={handleButtonClick}/>
     </div> 
-    {#each op as i}
-      {#if i === '1'}
-        {a} = i;
-      {/if}
-    {/each}
   </div>
 </main>
--- src/butt.svelte
+++ src/butt.svelte
@@ -1,11 +1,12 @@
 <script>
     export let items = [];
+    export let onButtonClick;
 </script>
 
 {#each items as item}
     <button 
-        on:click={()=> handleClick(item)}
         class="m-1 p-2 w-10 h-10 flex items-center justify-center bg-gray-700 hover:bg-gray-600 rounded border-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+        on:click={() => onButtonClick(item)}
     >
         {item}
     </button>
