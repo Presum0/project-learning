b8f5ab468ef057066adec79899b2c3cec860a5e8
Presum0 <preethamnelaturi@gmail.com>
2025-09-06 00:57:09 +0530

    input fixes

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,19 @@
   import But from "./butt.svelte";
   let inputValue = $state('0');
   let op = $state([1,2,3,4,5,6,7,8,9,0,'+','-','*','/','=','c']);
+  
+  function validateInput(value) {
+    // Only allow numbers, decimal point, and basic operators
+    return value.replace(/[^0-9+\-*/.\s]/g, '');
+  }
+  function handleresult() {
+    
+  }
   function handleButtonClick(value) {
     if (inputValue === '0' && value !== '.') {
       inputValue = value.toString();
+    } else if(value === 'c'){
+      inputValue = '0';
     } else {
       inputValue += value.toString();
     }
@@ -17,6 +27,7 @@
       type="text" 
       bind:value={inputValue} 
       class="w-full p-2 rounded bg-gray-800 text-white border border-gray-600 focus:border-blue-500 mb-1 focus:outline-none"
+      on:input={(e) => inputValue = validateInput(e.currentTarget.value)}
     >
     <div class="grid grid-cols-4 gap-1 mt-2">
       <But items={op} onButtonClick={handleButtonClick}/>
