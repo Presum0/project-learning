b704e8af81fd8ae816c90e07a052ea0d369c0aaf
Presum0 <preethamnelaturi@gmail.com>
2025-09-06 13:17:19 +0530

    Initiation of dollar to cents app, fixed the input section in a simpler manner

--- src/App.svelte
+++ src/App.svelte
@@ -3,5 +3,8 @@
 </script>
 
 <main class="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-900 text-white">
-  
+  <input 
+  type="number" 
+  class="w-32 p-2 bg-gray-800 text-white rounded border border-gray-600 focus:outline-none focus:border-blue-500 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+>
 </main>
